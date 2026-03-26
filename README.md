23572046d92b514d15de74165d39f26539e925c2
Joshua Miller <joshuamiller1@gmail.com>
2026-03-26 11:31:15 -0400

    Fixed README Conceal Level
    Now, bash ``` doesn't hid in the readme

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Config forked off of for [LazyVim](https://github.com/LazyVim/LazyVim).
 2. Clone this repo into `~/.config/nvim`
 3. Run `:Lazy sync` in Neovim to install plugins
 
-```
-
+```bash
+git Clone
 ```
--- init.lua
+++ init.lua
@@ -4,3 +4,9 @@ require("config.keymaps")
 require("plugins.transparent").setup() -- Example: set Material theme
 vim.g.material_style = "darker"
 vim.cmd("colorscheme tokyonight-night")
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+        vim.opt_local.conceallevel = 0
+    end,
+})
