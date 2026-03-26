e5c82963b2c3a2f85d4ae3e2a040fd8e8587dc18
Joshua Miller <joshuamiller1@gmail.com>
2026-03-26 11:05:50 -0400

    Initial Commit

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.keymaps")
 require("plugins.transparent").setup() -- Example: set Material theme
 vim.g.material_style = "darker"
 vim.cmd("colorscheme tokyonight-night")
