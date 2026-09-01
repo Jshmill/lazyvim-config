008f9a060754bc0c6da7f75f48e3de83a58e066b
Joshua Miller <joshuamiller1@gmail.com>
2026-09-01 12:09:21 -0400

    update plugins

--- lua/plugins/tsql.lua
+++ lua/plugins/tsql.lua
@@ -56,7 +56,7 @@ return {
         end,
     },
     {
-        "mason/mason.nvim",
+        "mason-org/mason.nvim",
         opts = function(_, opts)
             opts.ensure_installed = opts.ensure_installed or {}
             vim.list_extend(opts.ensure_installed, { "sqlfluff" })
