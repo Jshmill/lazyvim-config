80c6d7410efd5d7f3c5fd447cba6155e3a8d25ba
Joshua Miller <joshuamiller1@gmail.com>
2026-09-13 12:18:22 -0400

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,21 @@
+
 return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+        ensure_installed = {
+            "lua",
+            "python",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "json",
+            "zsh",
+            "tsql",
+        },
         highlight = {
             enable = true,
         },
     },
 }
+
