f204278dc0cebf7b9d42c9fff8a367461dbc6eda
Joshua Miller <joshua.miller@veracross.com>
2026-09-14 15:41:45 -0400

    yup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -28,7 +28,7 @@ vim.api.nvim_create_autocmd("Syntax", {
       "Coloring"
       syntax match @variable "@[A-Za-z_][A-Za-z0-9_]*" containedin=ALLBUT,Comment,String
       syntax keyword @tag DBO containedin=ALLBUT,Comment,String
-      syntax keyword @keyword.directive GO BEGIN END containedin=ALLBUT,Comment,String
+      syntax keyword @keyword.conditional GO BEGIN END containedin=ALLBUT,Comment,String
       syntax keyword @label INSERTED UPDATED DELETED containedin=ALLBUT,Comment,String
       syntax keyword @keyword.conditional IF ELSE THEN EXISTS RETURN containedin=ALLBUT,Comment,String
       syntax keyword @error RAISERROR containedin=ALLBUT,Comment,String
