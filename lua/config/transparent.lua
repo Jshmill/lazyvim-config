e45200de8e1ad96efd3de684bb4d32aad4403169
Joshua Miller <joshua.miller@veracross.com>
2026-09-16 09:29:12 -0400

    yup

--- lua/config/transparent.lua
+++ lua/config/transparent.lua
@@ -30,13 +30,13 @@ local function make_transparent()
         })
     end
 
-    local Directory = vim.api.nvim_get_hl(0, { name = "Directory" })
+    local Statement = vim.api.nvim_get_hl(0, { name = "Statement" })
 
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "FloatTitle", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "FloatFooter", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = Directory.fg, bg = "NONE", bold = true, italic = true })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = Statement.fg, bg = "NONE", bold = true, italic = true })
     vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "LineNr", { fg = 0xd3d3d3, bg = "NONE" })
     vim.api.nvim_set_hl(0, "LineNrAbove", { fg = 0xd3d3d3 })
